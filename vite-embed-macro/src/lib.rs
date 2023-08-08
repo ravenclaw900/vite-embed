5ca077b7fd3c9b5ccc4091f1f9f19c7237854f61
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-08-07 20:35:37 -0500

    Update to work with new iteration of DietPi-Dashboard

--- vite-embed-macro/src/lib.rs
+++ vite-embed-macro/src/lib.rs
@@ -15,6 +15,7 @@ pub(crate) fn unwrap_string_lit(lit: &Literal) -> String {
         panic!("Invalid string argument {}", lit_string)
     }
 
+    // Remove quotes at beginning and end
     lit_string.remove(0);
     lit_string.pop();
 
@@ -23,7 +24,7 @@ pub(crate) fn unwrap_string_lit(lit: &Literal) -> String {
 
 #[cfg(feature = "dev")]
 #[proc_macro]
-pub fn generate_vite_html_dev(tokens: TokenStream) -> TokenStream {
+pub fn generate_vite_dev(tokens: TokenStream) -> TokenStream {
     use dev::parse_tokens_html;
     use quote::quote;
 
@@ -43,10 +44,14 @@ pub fn generate_vite_html_dev(tokens: TokenStream) -> TokenStream {
         .as_str(),
     );
 
+    let html = html.as_bytes();
+
     let output = quote! {
-        fn vite_html_dev() -> &'static str {
-            #html
-        }
+        &[vite_embed::ViteData {
+            path: "/",
+            data: vite_embed::ViteFileData::UncompressedData(&[#(#html),*]),
+            mime_type: "text/html"
+        }]
     };
 
     TokenStream::from(output)
@@ -80,7 +85,7 @@ mod dev {
                 }
             }
             _ => panic!(
-                r#"Expected format generate_vite_html_dev!("/path/to/index.html", "vite-entry-point.js")"#
+                r#"Expected format generate_vite_html_dev!("/path/to/index.html", "vite-entry-point.tsx")"#
             ),
         }
     }
@@ -91,138 +96,145 @@ mod dev {
 pub fn generate_vite_prod(tokens: TokenStream) -> TokenStream {
     use prod::{compress, parse_tokens_prod};
     use quote::quote;
+    use serde_json::Value;
 
-    let macro_data = parse_tokens_prod(tokens);
+    let manifest_path = parse_tokens_prod(tokens);
 
-    let Ok(html) = std::fs::read_to_string(&macro_data.html_path) else {
-        panic!("Couldn't read {:?} as string", macro_data.html_path);
+    let Ok(manifest_string) = std::fs::read_to_string(&manifest_path) else {
+        panic!("Couldn't read {:?} as string", manifest_path);
     };
 
-    let Ok(manifest_string) = std::fs::read_to_string(&macro_data.manifest_path) else {
-        panic!("Couldn't read {:?} as string", macro_data.manifest_path);
+    let Ok(manifest_json) = serde_json::from_str::<Value>(&manifest_string) else {
+        panic!("Couldn't parse {:?} as JSON", manifest_path);
     };
 
-    let Ok(manifest_json) = json::parse(&manifest_string) else {
-        panic!("Couldn't parse {:?} as JSON", macro_data.manifest_path);
-    };
+    let entry_point = manifest_json
+        .as_object()
+        .unwrap()
+        .values()
+        .find(|&f| {
+            // If it exists, it has to be true
+            f["isEntry"].is_boolean()
+        })
+        .unwrap_or_else(|| panic!("No entry point in manifest.json"));
 
-    let mut file_names = Vec::new();
-
-    file_names.push("favicon.png");
-
-    let entry_point = &manifest_json[macro_data.entry_point];
-
-    if !entry_point["isEntry"].as_bool().unwrap_or(false) {
-        panic!("Wrong entry point");
-    }
+    // Default file names that won't be part of manifest.json
+    let mut file_names = vec!["index.html", "favicon.ico"];
 
     file_names.push(entry_point["file"].as_str().unwrap());
 
-    let html = html.replace(
-        "<!--vite-embed script injection-->",
-        &format!(r#"<script type="module" src="{}"></script>"#, file_names[1]),
-    );
-
-    let mut css_inject = String::new();
-
-    for i in entry_point["css"].members() {
-        file_names.push(i.as_str().unwrap());
-        css_inject.push_str(&format!(
-            r#"<link rel="stylesheet" href="{}" />
-            "#,
-            i.as_str().unwrap()
-        ));
+    if let Value::Array(arr) = &entry_point["css"] {
+        for i in arr {
+            file_names.push(i.as_str().unwrap());
+        }
     }
 
-    for i in entry_point["assets"].members() {
-        file_names.push(i.as_str().unwrap());
+    if let Value::Array(arr) = &entry_point["assets"] {
+        for i in arr {
+            file_names.push(i.as_str().unwrap());
+        }
     }
 
-    for i in entry_point["imports"].members() {
-        file_names.push(manifest_json[i.as_str().unwrap()]["file"].as_str().unwrap());
+    if let Value::Array(arr) = &entry_point["imports"] {
+        for i in arr {
+            file_names.push(manifest_json[i.as_str().unwrap()]["file"].as_str().unwrap());
+        }
     }
 
-    let html = html.replace("<!--vite-embed css injection-->\n", &css_inject);
-    let compressed_html = compress(html.as_bytes());
+    let mut mime_types = Vec::new();
+    for &i in &file_names {
+        mime_types.push(mime_guess::from_path(i).first_or_text_plain());
+    }
 
     let mut file_datas = Vec::new();
     let mut compressed_datas = Vec::new();
-    let parent_path = macro_data.manifest_path.parent().unwrap();
-    for i in &file_names {
+
+    let parent_path = manifest_path.parent().unwrap();
+    for (&name, mime) in file_names.iter().zip(mime_types.iter()) {
         let mut file_path = parent_path.to_path_buf();
-        file_path.push(i);
+        file_path.push(name);
         let Ok(file_data) = std::fs::read(&file_path) else {
             panic!("Couldn't read asset at {}", file_path.display());
         };
-        if file_path.extension().unwrap() == "png" {
-            file_datas.push(file_data);
-            compressed_datas.push(false);
-        } else {
-            let compressed = compress(&file_data);
-            file_datas.push(compressed);
-            compressed_datas.push(true);
+        match mime.type_() {
+            mime_guess::mime::IMAGE | mime_guess::mime::VIDEO => {
+                file_datas.push(file_data);
+                compressed_datas.push(false);
+            }
+            _ => {
+                let compressed = compress(&file_data);
+                file_datas.push(compressed);
+                compressed_datas.push(true);
+            }
         }
     }
 
-    // Append '/' to every item
-    let file_names = file_names
+    // Prepend '/' to every item for HTTP paths
+    let mut paths: Vec<_> = file_names
         .into_iter()
-        .map(|x| std::iter::once('/').chain(x.chars()).collect::<String>());
+        .map(|x| {
+            let mut x = x.to_string();
+            x.insert(0, '/');
+            x
+        })
+        .collect();
+
+    // Replace index.html with just / as path
+    // index.html should be at index 0 because that's how the Vec was created
+    paths[0] = "/".to_string();
+
+    let file_asts: Vec<_> = file_datas
+        .into_iter()
+        .zip(compressed_datas.into_iter())
+        .map(|(data, compressed)| {
+            if compressed {
+                quote! {
+                    vite_embed::ViteFileData::CompressedData(&[#(#data),*])
+                }
+            } else {
+                quote! {
+                    vite_embed::ViteFileData::UncompressedData(&[#(#data),*])
+                }
+            }
+        })
+        .collect();
+
+    // Have to get as Strings because quote can't work with Mime structs
+    let mime_types: Vec<_> = mime_types
+        .into_iter()
+        .map(|typ| typ.essence_str().to_string())
+        .collect();
 
     quote! {
-        fn vite_prod(path: &str) -> Option<(&'static [u8], bool)> {
-            match path {
-                "/index.html" => Some((&[#(#compressed_html),*], true)),
-                #(#file_names => Some((&[#(#file_datas),*], #compressed_datas))),*,
-                _ => None
-            }
-        }
+        &[#(vite_embed::ViteData {
+            path: #paths,
+            data: #file_asts,
+            mime_type: #mime_types
+        }),*]
     }
     .into()
 }
 
 #[cfg(feature = "prod")]
 mod prod {
-    use crate::{unwrap_string_lit, verify_sep};
+    use crate::unwrap_string_lit;
     use flate2::{write::GzEncoder, Compression};
     use proc_macro::{TokenStream, TokenTree};
     use std::io::Write;
     use std::path::PathBuf;
 
-    pub(super) struct MacroDataProd {
-        pub(super) manifest_path: PathBuf,
-        pub(super) entry_point: String,
-        pub(super) html_path: PathBuf,
-    }
-
-    pub(super) fn parse_tokens_prod(tokens: TokenStream) -> MacroDataProd {
+    pub(super) fn parse_tokens_prod(tokens: TokenStream) -> PathBuf {
         let token_vec: Vec<_> = tokens.into_iter().collect();
 
         match token_vec.as_slice() {
-            [TokenTree::Literal(manifest_token), TokenTree::Punct(sep1), TokenTree::Literal(entry_point_token), TokenTree::Punct(sep2), TokenTree::Literal(html_token)] =>
-            {
-                for i in [sep1, sep2].into_iter().enumerate() {
-                    verify_sep(i.1, (i.0, i.0 + 1))
-                }
-                let manifest_path = PathBuf::from(unwrap_string_lit(manifest_token).replace(
+            [TokenTree::Literal(manifest_token)] => {
+                PathBuf::from(unwrap_string_lit(manifest_token).replace(
                     "$CARGO_MANIFEST_DIR",
                     &std::env::var("CARGO_MANIFEST_DIR").unwrap(),
-                ));
-                let html_path = PathBuf::from(unwrap_string_lit(html_token).replace(
-                    "$CARGO_MANIFEST_DIR",
-                    &std::env::var("CARGO_MANIFEST_DIR").unwrap(),
-                ));
-                let entry_point = unwrap_string_lit(entry_point_token);
-                MacroDataProd {
-                    manifest_path,
-                    entry_point,
-                    html_path,
-                }
+                ))
             }
             _ => {
-                panic!(
-                    r#"Expected format generate_vite_prod!("/path/to/manifest.json", "vite-entry-point", "/path/to/index.html")"#
-                )
+                panic!(r#"Expected format generate_vite_prod!("/path/to/manifest.json")"#)
             }
         }
     }
--- vite-embed/src/lib.rs
+++ vite-embed/src/lib.rs
@@ -1,8 +1,23 @@
 #[cfg(feature = "dev")]
-pub use vite_embed_macro::generate_vite_html_dev;
+pub use vite_embed_macro::generate_vite_dev;
 
 #[cfg(feature = "prod")]
 pub use vite_embed_macro::generate_vite_prod;
+
+#[derive(Debug, Clone)]
+pub enum ViteFileData {
+    CompressedData(&'static [u8]),
+    UncompressedData(&'static [u8]),
+}
+
+#[derive(Debug, Clone)]
+pub struct ViteData {
+    pub path: &'static str,
+    pub data: ViteFileData,
+    pub mime_type: &'static str,
+}
+
+pub type ViteEmbed = &'static [ViteData];
 
 #[cfg(feature = "dev")]
 pub use ureq::Error as RequestError;
